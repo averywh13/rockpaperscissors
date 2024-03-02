5925f24c773b8051590a3d7fe3ef38cf830d9f8f
averywh13 <awekslerhart@gmail.com>
2024-03-02 16:05:29 -0500

    added outcomes, trying to get point system to work

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let userChoice;
 let compChoice;
 
-let userScore;
-let compScore;
+let userScore = 0;
+let compScore= 0;
 
+//prompt to ask username
 Window.onload = changeUsername();
-
 function changeUsername(element) {
   var username = prompt("Enter your username:"); // Asks for your username
   document.getElementById("username").innerHTML = username + ":"; // Displays Username
 }
+
+
+
 
 //when user clicks their character image --> character cards will change
 function rock() {
@@ -26,26 +29,37 @@ function rock() {
 }
 
 function paper() {
+  //get user choice
   userChoice = "paper";
-
+  //display user choice
   document.getElementById("userImg").src = `imgs/${userChoice}.webp`;
+  //get comp choice
   compChoice = getCompChoice();
+  //display comp choice
   document.getElementById("compImg").src = `imgs/${compChoice}.webp`;
-
+  //evaluate winners/losers
   playGame(userChoice, compChoice);
 }
 
 function scissors() {
+  //get user choice
   userChoice = "scissors";
-
+  //display user choice
   document.getElementById("userImg").src = `imgs/${userChoice}.webp`;
+  //get comp choice
   compChoice = getCompChoice();
+  //display comp choice
   document.getElementById("compImg").src = `imgs/${compChoice}.webp`;
+  //evaluate winners/losers
   playGame(userChoice, compChoice);
 }
 // ^^^^
 
-//function to randomly generate Comp Choice
+
+
+
+
+//function to randomly generate comp choice
 function getCompChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -57,22 +71,50 @@ function getCompChoice() {
   }
 }
 
+
+
+
+//outcome of characters
 function playGame(userChoice, compChoice) {
   if (userChoice === compChoice) {
-    return "This game is a tie!";
+    outcome.textContent = "Its a tie!";
+  } else if (
+    (userChoice === "rock" && compChoice === "scissors") ||
+    (userChoice === "scissors" && compChoice === "paper") ||
+    (userChoice === "paper" && compChoice === "rock")
+  ) {
+    outcome.textContent = `You win! Computer chose ${compChoice}.`;
+  } else {
+    outcome.textContent = `You lose! Computer chose ${compChoice}.`;
   }
-  if (userChoice === "rock") {
-    if (compChoice == "paper") {
-      return "Sorry, computer won!";
-    } else {
-      return "Congratulations, you won!";
-    }
-  }
-  if (userChoice === 'paper') {
-    if (compChoice == 'scissors') {
-      return "Sorry, computer won!";
-    }else {
-      return "Congratulations, you won!";
-      }
-    }
 }
+
+
+
+
+
+
+//scoreboard
+function playRound(userChoice) {
+if (  (userChoice === 'rock' && compChoice === 'scissors') ||
+      (userChoice === 'paper' && compChoice === 'rock') ||
+      (userChoice === 'scissors' && compChoice === 'paper') ){
+      outcomeDisplay.textContent = 'You win! Computer chose ${compChoice}.' ;
+      userScore++; // if player wins, point system will increase by one
+    } else {
+      outcomeDisplay.textContent = 'You lose! Computer chose ${compChoice}.';
+      compScore++; // if computer wins, point system will increase by one
+  }
+
+  userScoreDisplay.textContent = `Player Score: ${userScore}`; //display points for player
+  compScoreDisplay.textContent = `Computer Score: ${compScore}`;b //display points for computer
+}
+
+
+//reset button
+function reset(){
+  document.getElementById("userScore").innerHTML = 0;
+  document.getElementById("compScore").innerHTML = 0;
+}
+//when button is clicked, it will automatically set both computer and player's points to zero
+
